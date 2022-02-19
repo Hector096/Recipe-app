af3d017da24211a63bb100595676c5bd416c8714
Leandro-Barretoo <officialdevmi@gmail.com>
2022-02-19 17:24:22 +0000

    Shopping list fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,6 @@ class Ability
     can :read, Recipe, public: true
     can :manage, Food, user: user
     can :manage, Recipe, user: user
-    can :manage, Inventory, user: user
     return unless user
 
     can :manage, Ingredient if user
